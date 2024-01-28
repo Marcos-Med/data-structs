5f929af51de40438ffbb695c5c3c4c169839c2b7
Marcos <marcos.mb.mb@outlook.com>
2024-01-28 11:57:11 -0300

    create folder files_aux

---
+++ files_aux/functions_aux.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void imprimir_opcoes(){
+    printf("0 - Lista Sequencial\n");
+    printf("1 - Lista Sequencial Ordenada\n");
+    printf("2 - Lista Ligada\n");
+    printf("3 - Pilha\n");
+    printf("4 - Fila\n");
+    printf("5 - Deque\n");
+    printf("6 - Arvore Binaria\n");
+    printf("7 - Arvore Binaria de Busca\n");
+    printf("8 - Arvore AVL\n");
+    printf("9 - Sair\n");
+}
